151d2dfb8b60f4ac50e4159c97b1442a9dd09e2d
kendallcr04 <145626816+kendallcr04@users.noreply.github.com>
2025-02-12 15:19:31 -0600

    semana04

---
+++ tienda/src/main/java/com/tienda/projectconfig.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.tienda;
+
+/**
+ *
+ * @author Acer
+ */
+@configuration
+public class projectconfig implements webMvcConfigurer {
+@Configuration
+
+    /* Los siguientes métodos son para incorporar el tema de internacionalización en el proyecto */
+
+    /* localeResolver se utiliza para crear una sesión de cambio de idioma */
+    @Bean
+    public LocaleResolver localeResolver() {
+        var slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.getDefault());
+        slr.setLocaleAttributeName("session.current.locale");
+        slr.setTimeZoneAttributeName("session.current.timezone");
+
+        return slr;
+    }
+
+    /* localeChangeInterceptor se utiliza para crear un interceptor de cambio de idioma */
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    //Bean para poder acceder a los Messages.properties en código Java...
+    @Bean("messageSource")
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource= new ResourceBundleMessageSource();
+        messageSource.setBasenames("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+}
+}
